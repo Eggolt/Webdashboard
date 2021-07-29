e2ead2a82a6c5239df921a70833c5f535acf0b60
Eggolt <jae5142@gmail.com>
2021-07-28 20:19:17 -0400

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from . import models
 
-def get_coin(db: Session):
+def get_salary(db: Session):
     return db.query(models.salary.player, models.salary.fieldposition,
     models.salary.team, models.salary.salary).all()
